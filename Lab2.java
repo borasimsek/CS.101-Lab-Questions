7b4641ab25e1b0b97bf079c9b1bd51b5f69b1318
borasimsek <bora.simsek@ozu.edu.tr>
2022-02-19 17:16:31 +0300

    Psuedo code done

--- Lab2.java
+++ Lab2.java
@@ -8,18 +8,46 @@ public class Lab2 {
 
     /*
     The total price of a product is the price of the
-    product plus the sales tax. The sales tax is of a
+    product plus the sales tax. 
+    
+    The sales tax is of a
     product depends on the type of the product (an
-    integer). If the type of the product is 172, the sales
-    tax is 18%; if the type of the product is 188, the
-    sales tax is 8%; if the type of the product is 196,
-    the sales tax is 0%. Write a program that will read
+    integer). 
+    
+    If the type of the product is 172, the sales
+    tax is 18%;
+    
+    if the type of the product is 188, the
+    sales tax is 8%;
+    
+    if the type of the product is 196,
+    the sales tax is 0%.
+    
+    Write a program that will read
     the price of the product, the type of the product and
-    displays the total cost of the product. Your program
+    displays the total cost of the product.
+    
+    Your program
     will also display a message if the type of the product
     is incorrect
     */
     public static void question1() {
+        //Get the product price as integer and the type of the product as integer.
+        
+
+        //If the type is 172
+            // tax = Pprıce * 0.18
+
+        //If the type is 188
+            // tax = Pprıce * 0.8
+        
+        //If the type is 196
+            // tax = Pprıce * 0
+
+        // ıf the type other
+            // warn the user
+
+        // Dısplay the result
         
     }
     
