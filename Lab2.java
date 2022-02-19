6f65249b5c3749573c36e9ea8e14dceff1ecdb00
borasimsek <bora.simsek@ozu.edu.tr>
2022-02-19 17:08:33 +0300

    ilk soru eklendi

---
+++ Lab2.java
@@ -0,0 +1,26 @@
+public class Lab2 {
+    public static void main(String[] args) {
+        
+
+
+    }
+
+
+    /*
+    The total price of a product is the price of the
+    product plus the sales tax. The sales tax is of a
+    product depends on the type of the product (an
+    integer). If the type of the product is 172, the sales
+    tax is 18%; if the type of the product is 188, the
+    sales tax is 8%; if the type of the product is 196,
+    the sales tax is 0%. Write a program that will read
+    the price of the product, the type of the product and
+    displays the total cost of the product. Your program
+    will also display a message if the type of the product
+    is incorrect
+    */
+    public static void question1() {
+        
+    }
+    
+}
